6879c37da0ca6b86f47f9a2124c5b79709577e97
willy50414z <willy50414z@gmail.com>
2019-07-31 10:00:56 +0800

    加入SpringMVC

---
+++ Java/Springboot/SpringBootMVC/src/main/java/com/willy/sbmvc/controller/ControllerWithDiffReq.java
@@ -0,0 +1,31 @@
+package com.willy.sbmvc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerWithDiffReq {
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String loginGet() {
+	    return "Login Page";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String loginPost() {
+	    return "Login Post Request";
+	}
+	
+//	//新版MVC標記方式
+//	@GetMapping("/login")
+//	public String loginGet1() {
+//	    return "Login Page";
+//	}
+//
+//	@PostMapping("/login")
+//	public String loginPost1() {
+//	    return "Login Post Request";
+//	}
+}
---
+++ Java/Springboot/SpringBootMVC/src/main/java/com/willy/sbmvc/controller/ControllerWithJSP.java
@@ -0,0 +1,15 @@
+package com.willy.sbmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller//非文字回傳型別需用@Controller
+public class ControllerWithJSP {
+	@GetMapping("/gotoJSP")
+	public ModelAndView getoJSP() {
+		ModelAndView view = new ModelAndView("index");
+        view.addObject("userName", "Willy");
+        return view;
+	}
+}
