f908e3e8b49c3a334c81a42378c6c3e3ad3fe897
Willy Cheng <willy50414z@gmail.com>
2022-12-28 23:45:46 +0800

    add class binding sample

--- Angular/HelloWorld/src/app/app.component.ts
+++ Angular/HelloWorld/src/app/app.component.ts
@@ -13,4 +13,23 @@ export class AppComponent implements OnInit {
   }
   title = 'HelloWorld';
   placeholderProp = "placeholderValue"
+  onSave(event:MouseEvent){
+    alert("save success")
+    alert(event.clientX)
+  }
+
+  checkboxAll = false
+  checkbox1 = false
+  checkbox2 = false
+  toggleAll(){
+    this.checkboxAll = !this.checkboxAll;
+    this.checkbox1 = this.checkboxAll
+    this.checkbox2 = this.checkboxAll
+  }
+  toggleCheck1(){
+    this.checkbox1 = !this.checkbox1
+  }
+  toggleCheck2(){
+    this.checkbox2 = !this.checkbox2
+  }
 }
