30fb87124dc8fec7626822b86602144aa3d58c1e
Willy Cheng <willy50414z@gmail.com>
2023-04-01 22:30:26 +0800

    fix config setting

--- Java/Springboot/SpringClould/cloud/eureka-producer/src/main/java/com/willy/springboot/EurekaProducerApplication.java
+++ Java/Springboot/SpringClould/cloud/eureka-producer/src/main/java/com/willy/springboot/EurekaProducerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EurekaClient1Application
+public class EurekaProducerApplication
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(EurekaClient1Application.class, args);
+        SpringApplication.run(EurekaProducerApplication.class, args);
     }
 }
